e367dd96911d3b9b8aa9ce951a52267fd220a942
Charles Gerard <charles.l.gerard@gmail.com>
2022-11-18 12:25:53 -0500

    new endpoint

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare-api-s6cleqix5q-ew.a.run.app/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+// mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmxnZCIsImEiOiJjbGFtcjVtYmEwYTZoM3Fyd2gwbzY5ejE3In0.a9yAkfXn7-BnVciS0_K08A'
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
